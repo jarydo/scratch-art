ac8a9b1cdf1927bee0bf752694493bbf8fb0daed
jarydo <jarydnoahdiamond@gmail.com>
2025-06-30 00:16:53 -0400

    proxy debug 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://scratch-art-jaryd-diamonds-projects.vercel.app/",
 });
